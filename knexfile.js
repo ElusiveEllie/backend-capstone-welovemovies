1f4ae42e1b20c16cf86010313552b25b41df30ea
elusiveellie <elena@realemail.net>
2024-01-25 12:00:42 -0500

    setup backend and database migrations

---
+++ knexfile.js
@@ -0,0 +1,47 @@
+const path = require("path");
+
+require("dotenv").config();
+
+const {
+  DATABASE_URL = "postgres://bhxflqxi:cYHc7wk5_bwH0eSMSFU8oX1U_EyE5IWg@castor.db.elephantsql.com/bhxflqxi",
+} = process.env;
+
+module.exports = {
+  development: {
+    client: "postgresql",
+    connection: DATABASE_URL,
+    pool: { min: 0, max: 5 },
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+  },
+
+  production: {
+    client: "postgresql",
+    connection: DATABASE_URL,
+    pool: { min: 0, max: 5 },
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+  },
+
+  test: {
+    client: "sqlite3",
+    connection: {
+      filename: ":memory:",
+    },
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+    useNullAsDefault: true,
+  },
+};
